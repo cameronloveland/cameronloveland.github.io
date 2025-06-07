55105ba2d919f53e22944a85c862e66e66bbdaba
clov <clovola@gmail.com>
2025-06-07 17:41:10 -0500

    updating

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+ //output: 'export', // Uncomment this line if you want to export the app as a static site
   images: { unoptimized: true }, // Allows Next.js <Image> to work in static export
 };
 module.exports = nextConfig;
