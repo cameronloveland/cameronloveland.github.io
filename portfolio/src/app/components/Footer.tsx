57865bc3f0beeb6a0dcdad828497f6ce9e93ff6a
clov <clovola@gmail.com>
2025-06-22 21:58:42 -0500

    fix: remove header/footer borders

--- portfolio/src/app/components/Footer.tsx
+++ portfolio/src/app/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export function Footer() {
     return (
-        <footer className="fixed bottom-0 left-0 w-full px-6 py-4 flex items-center justify-between bg-neutral-950/80 text-neutral-400 text-sm border-t border-neutral-800 z-40">
-            <div className="flex-1">© {new Date().getFullYear()} Cameron Loveland</div>
+        <footer className=" text-xs fixed bottom-0 w-full px-6 py-4 flex items-center justify-between bg-neutral-500-950/40 text-neutral-400 text-sm  border-neutral-800 z-40">
+            <div className="flex-1 ">© {new Date().getFullYear()} Cameron Loveland</div>
             <div className="flex-1 text-right">
             </div>
         </footer >
